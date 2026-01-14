ffe194958f4cd5b64dc7881eeb2381ba0fad6553
melvyn920 <129944544+melvyn920@users.noreply.github.com>
2026-01-14 12:35:05 +0000

    u

--- app/src/tests/prisma-mock.ts
+++ app/src/tests/prisma-mock.ts
@@ -1,17 +1,8 @@
-import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
-
-import prisma from '../prisma/prisma-client';
 import { PrismaClient } from '@prisma/client';
+import { mockDeep } from 'jest-mock-extended';
 
-jest.mock('../prisma/prisma-client', () => ({
-  __esModule: true,
-  default: mockDeep<PrismaClient>(),
-}));
-
-const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
-
-beforeEach(() => {
-  mockReset(prismaMock);
-});
+// Remarque : on ne fait pas 'as unknown as DeepMockProxy<PrismaClient>'
+// car ça cause les problèmes de type circulaire avec Prisma 4+
+const prismaMock = mockDeep<PrismaClient>();
 
 export default prismaMock;
